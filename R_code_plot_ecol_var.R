2501ca06bc0a31bb1751b65c1d16715a74c16ff7
sofiamontanari <73693972+sofiamontanari@users.noreply.github.com>
2020-11-09 10:45:06 +0100

    Update R_code_plot_ecol_var.R

--- R_code_plot_ecol_var.R
+++ R_code_plot_ecol_var.R
@@ -23,4 +23,31 @@ plot(zinc,copper,col="green",pch=19,cex=2)
 #plot altogether!
 pairs(meuse)
 
+#lecture2
+#recall package and dataset
+library(sp)
+data(meuse)
 
+pairs(meuse)
+head(meuse)
+
+#cadmium copper lead zinc
+#pairs with soil variables
+
+pairs(meuse[,3:6])
+
+#use the columns' name 
+pairs(~ cadmium + copper + lead + zinc, data=meuse)
+
+#let's prettify the graph
+#Exercise: change the colour 
+
+pairs(meuse[,3:6], col="green")
+
+#Exercise: change the symbol to filled triangles
+
+pairs(meuse[,3:6], col="green", pch=17)
+
+#Exercise: increase the size of triangles
+
+pairs(meuse[,3:6], col="green", pch=17, cex=2)
