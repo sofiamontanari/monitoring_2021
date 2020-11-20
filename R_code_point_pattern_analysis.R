244348a53cf8fbde5f97388ae97f3483ad15db45
sofiamontanari <73693972+sofiamontanari@users.noreply.github.com>
2020-11-20 10:54:50 +0100

    Update R_code_point_pattern_analysis.R

--- R_code_point_pattern_analysis.R
+++ R_code_point_pattern_analysis.R
@@ -1,3 +1,26 @@
 #Point pattern analysis
+setwd("C:/lab/")
+covid <- read.table("covid_agg.csv", header= TRUE)
+covid
 
 install.packages("spatstat")
+library(spatstat)
+
+#density of the covid data 
+#planar point pattern in spatstat, before it is better to attache our dataset
+
+attach(covid)
+#x, y and ranges
+covid_planar <- ppp(lon, lat, c(-180, 180), c(-90, 90))
+
+density_map <- density(covid_planar)
+plot(density_map)
+points(covid_planar)
+
+#change colors, the 100 is related to the amount of color in the palette
+cl <- colorRampPalette(c('yellow', 'orange', 'red'))(100)
+plot(density_map, col= cl)
+
+#countries onto the map 
+install_package("rgdal")
+library(rgdal)
