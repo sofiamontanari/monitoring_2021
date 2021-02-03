ce59c2b1eab88519d18c7c9dfe2227f45dd5e228
sofiamontanari <73693972+sofiamontanari@users.noreply.github.com>
2021-02-03 20:26:32 +0100

    Update R_code_EXAM.r

--- R_code_EXAM.r
+++ R_code_EXAM.r
@@ -1,8 +1,10 @@
+#CONSEQUENCES OF 2019-2020 AUSTRALIAN BUSHFIRE SEASON ON VEGETATION AND CORRELATION WITH TEMPERATURE ANOMALY
+#maps downloaded from https://earthobservatory.nasa.gov/
+
 setwd("C:/lab/")
 
 library(raster)
 library(RStoolbox)
-
 
 #CHANGES IN NDVI (JAN 2019-JAN 2020)
 
@@ -19,7 +21,7 @@ ext <- c(3100,3400,400,800)
 NDVI19_au <- crop(NDVI19_1, ext)
 NDVI20_au <- crop(NDVI20_1, ext)
 
-#plot
+#change colors to highlight areas with high NDVI index and plot
 cl <- colorRampPalette(c('white', 'orange', 'red','darkred'))(100) 
 plot(NDVI19_au, col=cl)
 plot(NDVI20_au, col=cl)
@@ -27,7 +29,8 @@ dev.off()
 
 #difference between NDVI: jan2020 - jan2019
 difNDVI <- NDVI20_au - NDVI19_au
-cldif <- colorRampPalette(c('#FFFF00', '#ffe100', '#ff0000', '#0000ff', '#120078'))(100)
+cldif <- colorRampPalette(c('#FFFF00', '#ffe100', '#ff0000', '#0000ff', '#120078'))(100) #HEX color code
+#yellow (#FFFF00): areas with the highest decrease in NDVI
 plot(difNDVI, col=cldif)
 
 ##plot them together
@@ -45,44 +48,48 @@ NDVI <- stack(NDVI19_au, NDVI20_au)
 plot(NDVI)
 boxplot(NDVI,horizontal=T,axes=T,outline=F, col="lightgreen",xlab="NDVI", ylab="Year")
 
-#plot together
+#plot histogram and boxplot together
 par(mfrow=c(1,2))
 hist(difNDVI, col="lightgreen", xlab="Changes in NDVI", main= "Histogram")
 boxplot(NDVI,horizontal=T,axes=T,outline=F, col="lightgreen",xlab="NDVI", ylab="Year", main="Boxplot")
 
 #comparison with fire activity
-fire20 <- brick("FIRMS_2020.jpg")
-plotRGB(fire20, r=1, g=2, b=3, stretch="Lin")
-#plot together
+fire2020 <- raster ("fire2020.png") #upload
+fire2020_1 <- reclassify(fire2020, cbind(253:255, NA)) #remove ocean
+#crop australia
+ext <- c(3100,3400,400,800)
+fire2020_au <- crop(fire2020_1, ext)
+#change colors
+cl_fire <-colorRampPalette(c('grey', 'orange', 'red','darkred'))(100)
+plot(fire2020_au, col=cl_fire)
+
+#plot fire activity together with the difference in NDVI)
 par(mfrow=c(1,2))
-plotRGB(fire20, r=1, g=2, b=3, stretch="Lin")
 plot(difNDVI, col=cldif)
+plot(fire2020_au, col=cl_fire)
 
 
 #Temperature anomaly 
 
 #Images upload 
-
 TAN_global <- list.files(pattern="TAN")
 TAN_global
 list_TAN <- lapply(TAN_global, raster)
 list_TAN
  
 #CROP Australia
-#crop all the images at once: first I establish the extent (ext) and I create an empty list. Then, for every element IN list_TAN, 
+#crop all the images at once: first I establish the extent (ext) and I create an empty list. Then, for every element in list_TAN, 
 #I append to the empty list the cropped images with that extent.
-
 ext <- c(1100,1440,100,400)
 rasterList <- list()
 for (i in list_TAN) {  
  rasterList <- append(rasterList, crop(i, ext))
  }
 
-rasterList
+rasterList #see what is inside the list after the cycle
 
 #SAVE all the cropped images: cycle through raster list elements and save every raster object as .png
 #index is used to assign the progressive number to the file name
-
 index <- 1
 for(i in rasterList){
  fileName <- paste("TANau", index, ".png", sep="")
